197ea9c7fba1dc4a44f53305fb38b47eb9493050
mymb12 <idrisovmansur00@gmail.com>
2025-08-03 00:31:20 +0500

    added the pause/resume/quit/new commands to handle during listening the song

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use clap::Parser;
-use std::{io, sync::mpsc, thread};
+use std::{io, sync::mpsc, thread, time::Duration};
 
-mod cli;
-mod player;
+use crate::player::AudioPlayer;
 
-fn handle_keyboard_input(player: &mut player::AudioPlayer) {
+pub mod cli;
+pub mod player;
+
+/* fn handle_keyboard_input(player: &mut player::AudioPlayer) {
     println!("Starting receving the user input:\n1)resume\n2)pause\n3)new song");
     let (tx, rx) = mpsc::channel();
 
@@ -28,13 +30,62 @@ fn handle_keyboard_input(player: &mut player::AudioPlayer) {
     }
 
     println!("Exiting main thread");
+} */
+
+fn handle_keyboard_input(player: &mut AudioPlayer) {
+    println!("Starting receiving user input:");
+    println!("Commands: 'resume', 'pause', 'new <filename>', 'quit'");
+
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || loop {
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+
+        let input = input.trim().to_string();
+        if let Err(_) = tx.send(input) {
+            println!("Receiver dropped, exiting input thread.");
+            break;
+        }
+    });
+
+    loop {
+        if let Ok(received) = rx.try_recv() {
+            match received.as_str() {
+                "resume" => player.resume_audio(),
+                "pause" => player.pause_audio(),
+                "quit" => {
+                    println!("Exiting...");
+                    break;
+                }
+                input if input.starts_with("new ") => {
+                    let file_name = input.strip_prefix("new ").unwrap_or("");
+                    if !file_name.is_empty() {
+                        player.play_audio(&file_name.to_string());
+                    } else {
+                        println!("Please provide a filename: new <filename>");
+                    }
+                }
+
+                _ => println!(
+                    "Unknown command: '{}'. Available: resume, pause, new <filename>, quit",
+                    received
+                ),
+            }
+        }
+    }
+
+    println!("Exiting main thread");
 }
 
 fn main() {
     let args = cli::Cli::parse();
     let mut audio_player = player::AudioPlayer::new();
-    // handle_keyboard_input(&mut audio_player);
-    // ERROR: add an external thread for input procession
 
     audio_player.play_audio(&args.path);
+
+    handle_keyboard_input(&mut audio_player);
 }
--- src/player.rs
+++ src/player.rs
@@ -1,8 +1,7 @@
 use rodio;
 use rodio::Decoder;
+use std::fs::File;
 use std::io::BufReader;
-use std::path::PathBuf;
-use std::{fs::File, time::Duration};
 
 enum PlaybackState {
     Playing,
@@ -10,15 +9,9 @@ enum PlaybackState {
     Stopped,
 }
 
-enum PlayerCommand {
-    Play,
-    Pause,
-    // Stop,
-    LoadSong(PathBuf),
-}
-
 pub struct AudioPlayer {
     pub stream_handler: rodio::OutputStream,
+    pub sink: Option<rodio::Sink>,
     playback_state: PlaybackState,
 }
 
@@ -27,29 +20,70 @@ impl AudioPlayer {
         AudioPlayer {
             stream_handler: rodio::OutputStreamBuilder::open_default_stream()
                 .expect("open default audio system"),
+            sink: None,
             playback_state: PlaybackState::Stopped,
         }
     }
 
     pub fn resume_audio(&mut self) {
-        self.playback_state = PlaybackState::Playing;
-        println!("Audio resumed");
+        if let Some(sink) = &self.sink {
+            sink.play();
+            self.playback_state = PlaybackState::Playing;
+
+            println!("Song is resumed");
+        } else {
+            println!("No audio loaded to resume");
+        }
+    }
+
+    pub fn pause_audio(&mut self) {
+        if let Some(sink) = &self.sink {
+            sink.pause();
+            self.playback_state = PlaybackState::Paused;
+
+            println!("Song is paused");
+        } else {
+            println!("No audio loaded to pause");
+        }
     }
 
     pub fn play_audio(&mut self, file_name: &String) {
-        let file = File::open("songs/".to_string() + file_name).unwrap();
+        let file_path = "songs/".to_string() + file_name;
+        let file = match File::open(&file_path) {
+            Ok(file) => file,
+            Err(e) => {
+                println!("Failed to open file {}: {}", file_path, e);
+                return;
+            }
+        };
+
         let reader = BufReader::new(file);
-        // let _sink = rodio::play(&self.stream_handler.mixer(), file).unwrap();
-        let _sink = rodio::Sink::connect_new(&self.stream_handler.mixer());
+        let source = match Decoder::new(reader) {
+            Ok(source) => source,
+            Err(e) => {
+                println!("Failed to decode audio file: {}", e);
+                return;
+            }
+        };
 
-        let source = Decoder::new(reader).expect("Failed to decode mp3");
-        _sink.append(source);
+        let sink = rodio::Sink::connect_new(&self.stream_handler.mixer());
+        sink.append(source);
+        sink.set_volume(0.2);
 
+        self.sink = Some(sink);
         self.playback_state = PlaybackState::Playing;
 
-        _sink.set_volume(0.2);
-        _sink.sleep_until_end();
+        println!("Started playing: {}", file_name);
+    }
 
-        self.playback_state = PlaybackState::Stopped;
+    pub fn is_finished(&self) -> bool {
+        match &self.sink {
+            Some(sink) => sink.empty(),
+            None => true,
+        }
+    }
+
+    pub fn get_state(&self) -> &PlaybackState {
+        &self.playback_state
     }
 }
